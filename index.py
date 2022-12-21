8cb9a498c6dd17ee07d65db03d272c3266de3a9b
Chris <96923618+chrisdotz@users.noreply.github.com>
2022-12-21 17:22:45 -0600

    Create index.py

---
+++ index.py
@@ -0,0 +1,10 @@
+name = input("ENter name: ")
+name2 = input("ENter name: ")
+name3 = input("ENter name: ")
+
+roster = []
+roster.append(name)
+roster.append(name2)
+roster.append(name3)
+
+print(roster)
